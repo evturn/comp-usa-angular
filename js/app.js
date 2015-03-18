2bc8c3ac9518708a6737f8829eb4f4d891ba165e
Evan Turner <evturn@gmail.com>
2015-03-17 20:56:08 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+angular.module('compUSA', [])
+
+.controller('mainController', function() {
+
+	var vm = this;
+
+	vm.message = 'Welcome to Comp USA!';
+
+	vm.computers = [
+		{name: 'Toshiba', color: 'Black', ram: 64},
+		{name: 'Dell', color: 'Black', ram: 64},
+		{name: 'Fujitsu', color: 'Silver', ram: 64}
+	];
+})
